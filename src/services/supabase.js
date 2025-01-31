386baed4dd54c85cbee1bcfdf36747fd80af3cff
ade-tech <adelopoadekunle@gmail.com>
2025-01-31 21:28:44 +0100

    Made changes to my SupaBase File

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://lxpdxiknfwrjjpufgohp.supabase.co";
+export const supabaseUrl = "https://lxpdxiknfwrjjpufgohp.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4cGR4aWtuZndyampwdWZnb2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjUxMDYsImV4cCI6MjA1Mjk0MTEwNn0.oLDHDSS8s1inxOPrnF3W245VeHBlGU4-Uhovhd1QSxk";
 const supabase = createClient(supabaseUrl, supabaseKey);
